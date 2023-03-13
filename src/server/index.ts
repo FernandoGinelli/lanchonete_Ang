710263d1357297a4cd30db111949ab550580cf46
FernandoGinelli <fernandoginelli@gmail.com>
2023-03-13 15:52:18 -0300

    atualização bd

--- src/server/index.ts
+++ src/server/index.ts
@@ -4,21 +4,33 @@ import express from "express"
 import session from "cookie-session"
 import { auth } from "./auth"
 import { api } from "./api"
-
+import helmet from "helmet"
+import compression from "compression"
+import path from "path"
 
 
 
 
 
 const app = express()
-app.listen(3002, () => console.log("Server started"))
 
 app.use(
   session({
     secret: process.env["SESSION_SECRET"] || "my secret"
   })
 )
+
+app.use(helmet({ contentSecurityPolicy: false }))
+app.use(compression())
+
 app.use(api,auth)
+
+app.use(express.static(path.join(__dirname, "../remult-angular-todo")))
+app.get("/*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../remult-angular-todo", "index.html"))
+})
+
+app.listen(process.env["PORT"] || 3002, () => console.log("Server started"))
 
 
 
